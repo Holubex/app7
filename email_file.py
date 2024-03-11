a81501d11815b76588ed127c8d1032c3e160a5c4
User <kubo.holubek@gmail.com>
2024-03-11 12:51:12 +0100

    email_file.py

---
+++ email_file.py
@@ -0,0 +1,8 @@
+import yagmail
+
+email = yagmail.SMTP(user='holubek.kubo@gmail.com', password='xflcqaittnjbnhup')
+email.send(to='frdb47ba@spymail.one',
+           subject='HELLO',
+           contents='hhhhhhhhhhhhhh',
+           attachments='requirements.txt'
+           )
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class NewsFeed:
     base_url = "https://newsapi.org/v2/everything?"
     api_key = "ce0ae42b24e9451794188ce5c7d5e3a1"
 
-    def __init__(self, interest, from_date, to_date, language):
+    def __init__(self, interest: str, from_date: str, to_date: str, language: str):
         self.language = language
         self.to_date = to_date
         self.from_date = from_date
@@ -34,3 +34,11 @@ class NewsFeed:
 
         for article in articles:
             email_body = email_body + article["title"] + "\n" + article["url"] + "\n\n"
+
+        return email_body
+
+
+news_feed = NewsFeed(
+    interest="nasa", from_date="2024-03-09", to_date="2024-03-11", language="en"
+)
+print(news_feed.get())
