63e81b37a6932b2a6491126f98fd2c2f132376a0
User <kubo.holubek@gmail.com>
2024-03-11 10:26:53 +0100

    pridanie for loopy

--- main.py
+++ main.py
@@ -15,10 +15,18 @@ class NewsFeed:
 url = 'https://newsapi.org/v2/everything?'\
       'qInTitle=meditation&'\
       'from=2024-03-08&'\
-      'to=2024-03-01&'\
       'language=en&'\
+      'to=2024-03-01&'\
       'apiKey=ce0ae42b24e9451794188ce5c7d5e3a1'
 
-response = (requests.get(url))
-content = response.text
-pprint(content)
+response = requests.get(url)
+content = response.json()
+articles = content['articles']
+
+
+email_body = ''
+
+for article in articles:
+    email_body = email_body + article['title'] + '\n' + article['url'] + '\n\n'
+
+print(email_body)
